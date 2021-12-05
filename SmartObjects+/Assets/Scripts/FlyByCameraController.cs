f157294a85756b922981f91994cb558181eccf08
Brayan Lopez <blopezdev@gmail.com>
2021-12-04 20:39:21 -0800

    fixed camera from getting stuck not being able to move vertically

--- SmartObjects+/Assets/Scripts/FlyByCameraController.cs
+++ SmartObjects+/Assets/Scripts/FlyByCameraController.cs
@@ -5,38 +5,28 @@ using UnityEngine;
 public class FlyByCameraController : MonoBehaviour
 {
 
-    public float moveSpeed = 18; // the movement speed with WASD
-    public float maxCamHeight = 100;
-    public float minCamHeight = 0;
-    private Rigidbody myBody; 
+  public float moveSpeed = 18; // the movement speed with WASD
+  public float maxCamHeight = 100;
+  public float minCamHeight = 0;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        myBody = GetComponent<Rigidbody>();
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float xAxis = Input.GetAxisRaw("Horizontal");
-        float zAxis = Input.GetAxisRaw("Vertical");
-        float yAxis = Input.GetAxisRaw("Fire1");
+  // Update is called once per frame
+  void Update()
+  {
+    float xAxis = Input.GetAxisRaw("Horizontal");
+    float zAxis = Input.GetAxisRaw("Vertical");
+    float yAxis = Input.GetAxisRaw("Fire1");
 
-        Vector3 moveOffset = xAxis* transform.right+yAxis*transform.up+zAxis*transform.forward * moveSpeed * Time.unscaledDeltaTime;
-        //Vector3 moveOffset = new Vector3(xAxis, yAxis, zAxis) * moveSpeed * Time.deltaTime;
-
-
-        if (((transform.position + moveOffset).y < minCamHeight) || ((transform.position + moveOffset).y > maxCamHeight))
-        {
-            moveOffset.y = 0;
-        }
+    Vector3 moveOffset = xAxis * transform.right + yAxis * transform.up + zAxis * transform.forward * moveSpeed * Time.unscaledDeltaTime;
+    //Vector3 moveOffset = new Vector3(xAxis, yAxis, zAxis) * moveSpeed * Time.deltaTime;
 
 
-        myBody.transform.position = transform.position + moveOffset;
-        //myBody.MovePosition(transform.position + moveOffset);
-
-      
-    }
+    transform.position += moveOffset;
+    transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minCamHeight, maxCamHeight),transform.position.z);
+  }
 }
